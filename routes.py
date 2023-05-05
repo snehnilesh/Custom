992468f032f8db04130c5ab59946e20431ad2760
snehnilesh <86720256+snehnilesh@users.noreply.github.com>
2023-05-05 10:37:48 +0100

    first commit

---
+++ routes.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="192.168.0.12")
+
